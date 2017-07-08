f471b484eed89a81749965bfae614f131737c9cd
Liam Wilson <cosinusoidally@gmail.com>
2017-07-08 16:36:27 +0000

    On load change loading message to a console.warn

--- napi-compatibility-module-loader-loader.js
+++ napi-compatibility-module-loader-loader.js
@@ -1,4 +1,4 @@
-console.log("Loading node-napi-compatibility-module");
+console.warn("Loading node-napi-compatibility-module");
 b=process.env.NAPI_INSTALL_DIR;
 b=b+"/napi-compatibility-module-loader.node";
 a=require(b);
